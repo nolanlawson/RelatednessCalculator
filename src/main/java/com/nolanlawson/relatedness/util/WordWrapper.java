a2929df9a3842cc14dcecbb66267e0a161874cab
Nolan Lawson <nolan.lawson@gmail.com>
2012-06-09 12:57:26 +0200

    unused imports

--- src/main/java/com/nolanlawson/relatedness/util/WordWrapper.java
+++ src/main/java/com/nolanlawson/relatedness/util/WordWrapper.java
@@ -2,7 +2,6 @@ package com.nolanlawson.relatedness.util;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.regex.Matcher;
